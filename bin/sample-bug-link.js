5542879593d2e9ad6d89bcd334e994cc5069f6af
John Ford <john@johnford.info>
2013-09-11 15:12:30 +0200

    use json for config data

--- bin/sample-bug-link.js
+++ bin/sample-bug-link.js
@@ -3,20 +3,14 @@ var fs = require('fs');
 var bz_glue = require('../lib/api_glue/bz');
 var github_glue = require('../lib/api_glue/github');
 var linkify = require('../lib/linkify');
+var config = require('../ghb.json');
 
-var ini_file = 'ghb.ini';
 
 
-fs.exists(ini_file, function(exists) {
-    if (exists) {
-        bz = bz_glue.init(ini_file);
-        github = github_glue.init(ini_file);
-        linkify.link(bz, github, 'staging-mozilla-b2g', 'gaia', 2,
-            function(err, at_id) {
-                console.log(at_id);
-            }
-        );
-    } else {
-        console.log('config file (' + ini_file + ') doesn\'t exist');
+bz = bz_glue.init(config.bugzilla);
+github = github_glue.init(config.github);
+linkify.link(bz, github, 'staging-mozilla-b2g', 'gaia', 2,
+    function(err, at_id) {
+        console.log(at_id);
     }
-});
+);
